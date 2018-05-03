077cecd87f46cb989489b3b03bb81e351a90caa5
nolawill <will@sleekdesignstudio.com>
2018-05-03 08:26:12 -0500

    sample algolia vue app with typescript declaration issue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <ais-index
+    app-id="latency"
+    api-key="3d9875e51fbd20c7754e65422f7ce5e1"
+    index-name="bestbuy"
+  >
+    <ais-search-box></ais-search-box>
+    <ais-results>
+      <template slot-scope="{ result }">
+        <h2>
+          <ais-highlight :result="result" attribute-name="name"></ais-highlight>
+        </h2>
+      </template>
+    </ais-results>
+  </ais-index>
 </template>
 
 <style lang="scss">
